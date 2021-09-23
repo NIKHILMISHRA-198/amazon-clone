32af0fb145f8cebe1886181c19c3e5d4c07f33df
NIKHILMISHRA-198 <56644226+NIKHILMISHRA-198@users.noreply.github.com>
2021-09-23 23:40:13 +0530

    cart-1

--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,14 @@ function Home() {
 
                 <div className="home__row">
                     <Product
+                        id="123456"
                         title='The Making of Hero: Four Brothers, Two Wheels and a Revolution that Shaped India
                         The Making of Hero: Four Brothers, Two Wheels and a Revolution that Shaped India'
                         price={69.99}
                         image="https://images-na.ssl-images-amazon.com/images/I/91jFbbawmdL.jpg"
                         rating={5} />
                     <Product
+                        id="123451"
                         title='Amazon Basics Charging Station Dock for 4 Nintendo Switch Joy-con Controllers - 2.6 Foot Cable, Black'
                         price={81.99}
                         image="https://m.media-amazon.com/images/I/71SQSraI7rL._AC_UL480_FMwebp_QL65_.jpg"
@@ -25,18 +27,21 @@ function Home() {
 
                 <div className="home__row">
                     <Product
+                        id="123452"
                         title='Mellanni Split King Sheet Set for Adjustable Bed - Hotel Luxury 1800 Bedding Sheets & Pillowcases - Extra Soft Cooling Bed Sheets - Deep...'
                         price={20.99}
                         image="https://m.media-amazon.com/images/I/61YuSrsuf4L._AC_UL480_FMwebp_QL65_.jpg"
                         rating={3} />
 
                     <Product
+                        id="123453"
                         title='Marshall Stanmore II Wireless Bluetooth Speaker, Black - NEW'
                         price={80.99}
                         image="https://m.media-amazon.com/images/I/71VfR7Xhk8L._AC_UL480_FMwebp_QL65_.jpg"
                         rating={3} />
 
                     <Product
+                        id="123454"
                         title='2019 Apple MacBook Pro (16-inch, 16GB RAM, 1TB Storage, 2.3GHz Intel Core i9) - Space Gray'
                         price={20.99}
                         image="https://m.media-amazon.com/images/I/71pC69I3lzL._AC_UY327_FMwebp_QL65_.jpg"
@@ -45,6 +50,7 @@ function Home() {
 
                 <div className="home__row">
                     <Product
+                        id="123457"
                         title='SAMSUNG 50-Inch Class Crystal UHD AU8000 Series - 4K UHD HDR Smart TV with Alexa Built-in (UN50AU8000FXZA, 2021 Model)'
                         price={2050}
                         image="https://m.media-amazon.com/images/I/71LJJrKbezL._AC_UY327_FMwebp_QL65_.jpg"
--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './Product.css';
+import { useStateValue } from "./StateProvider"
 
+function Product({ title, image, price, rating, id }) {
+    const [{ basket }, dispatch] = useStateValue();
 
-function Product({ title, image, price, rating }) {
+    console.log("this is the basket", basket)
+
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
+
     return (
         <div
             className="product">
@@ -23,7 +41,7 @@ function Product({ title, image, price, rating }) {
             <img
                 src={image} alt="" />
 
-            <button >Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
