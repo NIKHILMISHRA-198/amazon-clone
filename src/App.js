4c0338a4bdea9591e9b8090d65a869bdf402fbf7
NIKHILMISHRA-198 <56644226+NIKHILMISHRA-198@users.noreply.github.com>
2021-09-29 18:57:18 +0530

    before-payment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from './firebase'
 import { useStateValue } from "./StateProvider";
-
+import Payment from "./Payment";
 
 
 function App() {
@@ -55,6 +55,14 @@ function App() {
             <Checkout />
 
           </Route>
+
+          <Route path="/payment">
+            <Header />
+            <Payment />
+
+          </Route>
+
+
           <Route path="/">
             <Header />
             <Home />
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,7 +3,14 @@ import './Subtotal.css'
 import CurrencyFormat from "react-currency-format"
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router';
+
+
+
+
 function Subtotal() {
+
+    const history = useHistory();
     const [{ basket }, dispatch] = useStateValue();
 
     return (
@@ -28,7 +35,7 @@ function Subtotal() {
                 prefix={"$"}
 
             />
-            <button>Proceed to Checkout </button>
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout </button>
         </div>
     )
 }
