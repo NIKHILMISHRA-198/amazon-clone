f7190f8c10dc883205689422cb9b60969c46a3b9
NIKHILMISHRA-198 <56644226+NIKHILMISHRA-198@users.noreply.github.com>
2021-10-01 09:33:03 +0530

    final deploy

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/challenge-bc1ba/us-central1/api' // THE API (cloud function) URL
+    baseURL: 'https://us-central1-challenge-bc1ba.cloudfunctions.net/api' // THE API (cloud function) URL
+    // baseURL: 'http://localhost:5001/challenge-bc1ba/us-central1/api' // THE API (cloud function) URL
 });
 
 export default instance;
